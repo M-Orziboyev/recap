bc0dbe22dd7368c5064986fa1454ddba052bd436
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2025-01-02 09:29:27 +0500

    iteration

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-function greetings (callback) {
-    console.log('Hello guys');
+// Object va destructor
 
-    callback()
+const person = {
+    name: 'Muffy',
+    age: '17',
+    job: 'Software Engineer',
+    hobbies: {
+        sport: 'football',
+        games: 'UFC'
+    }
 }
-greetings(() => console.log('This is a arrow function'))
+// How to know length of our object
+
+console.log(Object.keys(person).length)
+
+// for in --> objectlar uchun
+//for of --> array va massivlar uchun
+
+for( let key in person){
+    if(typeof person[key] === 'object'){
+        for (let i in person[key]){
+            console.log(`Property ${i}: value ${person[key][i]}`)
+        }
+    }else{
+        console.log(`Property ${key}: value ${person[key]}`)
+    }
+}
