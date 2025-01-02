a5ba0febac207d5ccabfe9ea3039ed9c4da95ac2
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2025-01-02 09:32:38 +0500

    I create my own method in  object

--- index.js
+++ index.js
@@ -7,21 +7,27 @@ const person = {
     hobbies: {
         sport: 'football',
         games: 'UFC'
+    },
+    sayHello: function () {
+        console.log(this.name)
     }
 }
+console.log(person)
+
+person.sayHello()
 // How to know length of our object
 
-console.log(Object.keys(person).length)
+// console.log(Object.keys(person).length)
 
 // for in --> objectlar uchun
 //for of --> array va massivlar uchun
 
-for( let key in person){
-    if(typeof person[key] === 'object'){
-        for (let i in person[key]){
-            console.log(`Property ${i}: value ${person[key][i]}`)
-        }
-    }else{
-        console.log(`Property ${key}: value ${person[key]}`)
-    }
-}
+// for( let key in person){
+//     if(typeof person[key] === 'object'){
+//         for (let i in person[key]){
+//             console.log(`Property ${i}: value ${person[key][i]}`)
+//         }
+//     }else{
+//         console.log(`Property ${key}: value ${person[key]}`)
+//     }
+// }
