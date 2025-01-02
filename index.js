aeeb57c3d91100a14625c6261cb902714f981ea2
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2025-01-02 14:10:17 +0500

    massive

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
-// Object va destructor
+// Massive
 
-const person = {
-    name: 'Muffy',
-    age: '17',
-    job: 'Software Engineer',
-    hobbies: {
-        sport: 'football',
-        games: 'UFC'
-    },
-    sayHello: function () {
-        console.log(`Hello ${this.name}`)
-    }
-}
-// console.log(person)
-//
-// person.sayHello()
+const colors = ['red', 'blue', 'green', 'yellow', 'brown']
 
-const {name, age,job, hobbies: {sport, games}, sayHello} = person;
+console.log(`Massive uzunligi: ${colors.length}`)
+console.log(`Index yordamida element olish: ${colors[2]}`)
+console.log(`Oxirigi elementni o'chiradi: ${colors.pop()}`)
+console.log(`Oxiriga bitta yangi element qo'shadi ${colors.push('gray')}`)
+// console.log(`Oldidagi elementni o'chiradi: ${colors.shift()}`)
+// console.log(`Oldidan bitta yangi element qo'shadi ${colors.unshift('gray')}`)
 
-sayHello()
-// How to know length of our object
+//Pop va Push
+//Pop --> bu massivni oxirigi elementini o'chiradi
+//Push --> bu massivning oxiriga bitta yangi element qo'shadi
+//Shift va Unshift
+//Shift --> bu massivni oxirigi elementini o'chiradi
+//Unshift --> bu massivning oxiriga bitta yangi element qo'shadi
 
-// console.log(Object.keys(person).length)
 
-// for in --> objectlar uchun
-//for of --> array va massivlar uchun
+console.log(colors)
 
-// for( let key in person){
-//     if(typeof person[key] === 'object'){
-//         for (let i in person[key]){
-//             console.log(`Property ${i}: value ${person[key][i]}`)
-//         }
-//     }else{
-//         console.log(`Property ${key}: value ${person[key]}`)
-//     }
+
+
+const cars = ['BMW', 'Audi', 'Subaru', 'Suzuki', 'Mers']
+//Iteration
+
+// for (let i = 0; i < cars.length; i++){
+//     console.log(cars[i])
 // }
+
+// for (let car of cars){
+//     console.log(car)
+// }
+
+// colors.forEach((item, index, arr) => {
+//     console.log(`${index}: ${item}`)
+// })
+
+const numbers = [12,45,23,55]
+
+numbers.sort(compareFn)
+
+function compareFn(a, b){
+    return a - b
+}
+
+console.log(numbers)
+
+
