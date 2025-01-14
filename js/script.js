df4314a92c0fd91f763308d3286894935af762e0
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2025-01-14 16:58:02 +0500

    constructor function

--- js/script.js
+++ js/script.js
@@ -1,37 +1,50 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const tabs = document.querySelectorAll('.tabheader__item'),
-        tabContents = document.querySelectorAll('.tab_content'),
-        tabParents = document.querySelector('.tabheader__items');
+// window.addEventListener('DOMContentLoaded', () => {
+//     const tabs = document.querySelectorAll('.tabheader__item'),
+//         tabContents = document.querySelectorAll('.tab_content'),
+//         tabParents = document.querySelector('.tabheader__items');
+//
+//     function hideContent() {
+//         tabContents.forEach(content => {
+//             content.classList.add('hide')
+//             content.classList.remove('show')
+//         })
+//
+//         tabs.forEach(tab => {
+//             tab.classList.remove('tabheader__item_active')
+//         })
+//     }
+//
+//     function showContent(index = 0) {
+//         tabContents[index].classList.add('show', 'fade')
+//         tabContents[index].classList.remove('hide')
+//         tabs[index].classList.add('tabheader__item_active')
+//     }
+//
+//     hideContent()
+//     showContent()
+//
+//     tabParents.addEventListener('click', () => {
+//         const target = event.target
+//         if (target && target.classList.contains('tabheader__item')){
+//             tabs.forEach((tab, index) => {
+//                 if (tab === target){
+//                     hideContent()
+//                     showContent(index)
+//                 }
+//             })
+//         }
+//     })
+// })
 
-    function hideContent() {
-        tabContents.forEach(content => {
-            content.classList.add('hide')
-            content.classList.remove('show')
-        })
-
-        tabs.forEach(tab => {
-            tab.classList.remove('tabheader__item_active')
-        })
+function Person(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.hello = function(){
+        console.log(`hello ${firstName} ${lastName}`)
     }
+}
 
-    function showContent(index = 0) {
-        tabContents[index].classList.add('show', 'fade')
-        tabContents[index].classList.remove('hide')
-        tabs[index].classList.add('tabheader__item_active')
-    }
+const firstPerson = new Person('Muzaffar', 'Orziboyev')
 
-    hideContent()
-    showContent()
-
-    tabParents.addEventListener('click', () => {
-        const target = event.target
-        if (target && target.classList.contains('tabheader__item')){
-            tabs.forEach((tab, index) => {
-                if (tab === target){
-                    hideContent()
-                    showContent(index)
-                }
-            })
-        }
-    })
-})
+// console.log(firstPerson)
+firstPerson.hello()
