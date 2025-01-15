d415c0d4ee8779907656a3b20ed2bc9ac1af4805
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2025-01-15 16:55:28 +0500

    Context this

--- js/script.js
+++ js/script.js
@@ -1,58 +1,71 @@
-// window.addEventListener('DOMContentLoaded', () => {
-//     const tabs = document.querySelectorAll('.tabheader__item'),
-//         tabContents = document.querySelectorAll('.tab_content'),
-//         tabParents = document.querySelector('.tabheader__items');
-//
-//     function hideContent() {
-//         tabContents.forEach(content => {
-//             content.classList.add('hide')
-//             content.classList.remove('show')
-//         })
-//
-//         tabs.forEach(tab => {
-//             tab.classList.remove('tabheader__item_active')
-//         })
-//     }
-//
-//     function showContent(index = 0) {
-//         tabContents[index].classList.add('show', 'fade')
-//         tabContents[index].classList.remove('hide')
-//         tabs[index].classList.add('tabheader__item_active')
-//     }
-//
-//     hideContent()
-//     showContent()
-//
-//     tabParents.addEventListener('click', () => {
-//         const target = event.target
-//         if (target && target.classList.contains('tabheader__item')){
-//             tabs.forEach((tab, index) => {
-//                 if (tab === target){
-//                     hideContent()
-//                     showContent(index)
-//                 }
-//             })
-//         }
-//     })
+// Context this
+
+// function showThis() {
+// 	console.log(this)
+// }
+
+
+// showThis()
+
+// const person = {
+// 	firstName: "Samar",
+// 	lastName: "Badriddinov",
+// 	greeting: function() {
+// 		const showThis = () => {
+// 			console.log(this)
+// 		}
+// 		showThis()
+// 		console.log(this)
+// 	}
+// }
+
+// person.greeting()
+
+// function Person(firstName, lastName) {
+// 	this.firstName = firstName
+// 	this.lastName = lastName
+// 	this.isHuman = true
+// 	this.greeting = () => {
+// 		console.log(this)
+// 	}
+// }
+
+// const firstPerson = new Person("Abdulloh", "Mahmud")
+
+// console.log(firstPerson.greeting())
+
+// function greeting(isMarried) {
+// 	console.log(this.name + "Married: " + isMarried)
+// }
+
+// const person = {
+// 	name: "Samar Badriddinov"
+// }
+
+// greeting.call(person, true)
+// greeting.apply(person, [false])
+
+// function calc(number) {
+// 	return this*number
+// }
+
+// const double = calc.bind(2)
+// const trouble = calc.bind(3)
+
+// console.log(double(12))
+// console.log(double(24))
+
+// console.log(trouble(15))
+// console.log(trouble(30))
+
+// #1. Oddiy functiondagi context this window obyektga teng, qat'iy rejim ishlasa undefined
+// #2. Kontext obyektlardagi method - obyektga teng,
+//     Arrow functiondagi context this - uni yuqoridagi contextga osilib oladi
+// #3. Context this function constructorni ichida - yangi obyektni ekzemplyar
+// #4. Call, Apply va Bind methodlar bu functionga context bog'lash
+
+// const btn = document.querySelector("button")
+
+// btn.addEventListener("click", function(evt) {
+// 	evt.target.style.background = "red"
 // })
-
-function Person(firstName, lastName) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.hello = function () {
-        console.log(`hello ${firstName} ${lastName}`)
-    }
-}
-
-Person.prototype.convertAge = function (age) {
-    console.log(`${this.firstName} age is: ${age}`)
-}
-
-const firstPerson = new Person('Muzaffar', 'Orziboyev')
-const secondPerson = new Person('Umar', 'Abdurahmonov')
-
-// console.log(firstPerson)
-firstPerson.hello()
-secondPerson.hello()
-firstPerson.convertAge(17)
-secondPerson.convertAge(20)
