3f259d0d9d309b9da2e9cfc4c4a960b70572370a
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2025-01-16 16:49:27 +0500

    Class

--- js/script.js
+++ js/script.js
@@ -1,71 +1,27 @@
-// Context this
+//class
 
-// function showThis() {
-// 	console.log(this)
-// }
+class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    greetings(){
+        return `Here ${this.firstName} ${this.lastName}`
+    }
+}
 
 
-// showThis()
+class StatusPeople extends Person {
+    constructor(firstName, lastName, isMarried) {
+        super(firstName, lastName);
+        this.isMarried = isMarried
+    }
+    get(){
+        return `${this.firstName} ${this.lastName} Married: ${this.isMarried}`
+    }
+}
+const firstPerson = new Person('Muzaffar', 'Orziboyev')
+const secondPerson = new StatusPeople('Muzaffar', 'Orziboyev', false)
+console.log(firstPerson.greetings())
+console.log(secondPerson.get())
 
-// const person = {
-// 	firstName: "Samar",
-// 	lastName: "Badriddinov",
-// 	greeting: function() {
-// 		const showThis = () => {
-// 			console.log(this)
-// 		}
-// 		showThis()
-// 		console.log(this)
-// 	}
-// }
-
-// person.greeting()
-
-// function Person(firstName, lastName) {
-// 	this.firstName = firstName
-// 	this.lastName = lastName
-// 	this.isHuman = true
-// 	this.greeting = () => {
-// 		console.log(this)
-// 	}
-// }
-
-// const firstPerson = new Person("Abdulloh", "Mahmud")
-
-// console.log(firstPerson.greeting())
-
-// function greeting(isMarried) {
-// 	console.log(this.name + "Married: " + isMarried)
-// }
-
-// const person = {
-// 	name: "Samar Badriddinov"
-// }
-
-// greeting.call(person, true)
-// greeting.apply(person, [false])
-
-// function calc(number) {
-// 	return this*number
-// }
-
-// const double = calc.bind(2)
-// const trouble = calc.bind(3)
-
-// console.log(double(12))
-// console.log(double(24))
-
-// console.log(trouble(15))
-// console.log(trouble(30))
-
-// #1. Oddiy functiondagi context this window obyektga teng, qat'iy rejim ishlasa undefined
-// #2. Kontext obyektlardagi method - obyektga teng,
-//     Arrow functiondagi context this - uni yuqoridagi contextga osilib oladi
-// #3. Context this function constructorni ichida - yangi obyektni ekzemplyar
-// #4. Call, Apply va Bind methodlar bu functionga context bog'lash
-
-// const btn = document.querySelector("button")
-
-// btn.addEventListener("click", function(evt) {
-// 	evt.target.style.background = "red"
-// })
