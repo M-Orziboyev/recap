6910889b84e639ef2dd4934645e3952c5ca447f6
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2025-02-19 15:10:53 +0500

    Promise

--- js/script.js
+++ js/script.js
@@ -1,17 +1,43 @@
-const sum = document.querySelector('#sum'),
-    usd = document.querySelector('#usd');
+const status = 'success'
 
-
-sum.addEventListener('input', () => {
-    const request = new XMLHttpRequest()
-
-    request.open('GET', "./js/current.json")
-    request.setRequestHeader('Content-Type', 'application/json')
-    request.send()
-
-    request.addEventListener('load', () => {
-        if (request.status === 200) {
-            console.log(request.response)
-        }
-    })
+const request = new Promise((resolve, reject) => {
+    if (status === 'success') {
+        resolve()
+    } else {
+        reject(new Error)
+    }
 })
+
+
+request
+    .then(() => {
+        console.log('Starting the new position')
+    })
+    .then(() => {
+        console.log('Something really good happened')
+    })
+    .catch(() => {
+        console.log('Something really bad happened')
+    })
+    .finally(() => {
+        console.log('Anyway, you are muslim so say Alhamdulillah')
+    })
+
+
+const logger = time => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve()
+        }, time)
+    })
+}
+
+// logger(1000).then(() => {
+//     console.log('1000ms')
+// })
+// logger(2000).then(() => {
+//     console.log('2000ms')
+// })
+
+Promise.all([logger(1000), logger(2000)]).then(() => {console.log('All promises was resolved')})
+Promise.race([logger(1000), logger(2000)]).then(() => {console.log('All promises was resolved')})
