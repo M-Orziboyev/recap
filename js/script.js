5a33f8163d8bfae5061b11e1d8cc6602d2da6803
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2025-02-20 16:50:03 +0500

    post method

--- js/script.js
+++ js/script.js
@@ -1,51 +1,7 @@
-const status = 'success'
-
-const request = new Promise((resolve, reject) => {
-    if (status === 'success') {
-        resolve()
-    } else {
-        reject(new Error)
-    }
-})
-
-
-// request
-//     .then(() => {
-//         console.log('Starting the new position')
-//     })
-//     .then(() => {
-//         console.log('Something really good happened')
-//     })
-//     .catch(() => {
-//         console.log('Something really bad happened')
-//     })
-//     .finally(() => {
-//         console.log('Anyway, you are muslim so say Alhamdulillah')
-//     })
-
-
-// const logger = time => {
-//     return new Promise(resolve => {
-//         setTimeout(() => {
-//             resolve()
-//         }, time)
-//     })
-// }
-//
-// // logger(1000).then(() => {
-// //     console.log('1000ms')
-// // })
-// // logger(2000).then(() => {
-// //     console.log('2000ms')
-// // })
-//
-// Promise.all([logger(1000), logger(2000)]).then(() => {console.log('All promises was resolved')})
-// Promise.race([logger(1000), logger(2000)]).then(() => {console.log('All promises was resolved')})
-
 window.addEventListener('DOMContentLoaded', () => {
     const postWrapper = document.querySelector('.posts')
 
-    fetch('https://jsonplaceholder.typicode.come/posts', {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
         method: 'GET',
         headers: {'Content-Type': 'application/json'}
     }).then(response => response.json())
@@ -53,22 +9,51 @@ window.addEventListener('DOMContentLoaded', () => {
             data.forEach((item, index) => {
                 const postEl = document.createElement('div');
                 postEl.classList.add('post')
-                postEl.innerHTML = `
+                postEl.innerHTML += `
                 <h4> <b>${index + 1}</b> ${item.title}</h4>
                 <p>${item.body}</p>
               `
-
                 postWrapper.append(postEl)
             })
-
         }).catch(() => {
-            const error = document.createElement('div')
-            error.classList.add('error')
-            error.innerHTML = 'Something went wrong with the network connections'
+        const error = document.createElement('div')
+        error.classList.add('error')
+        error.innerHTML = 'Something went wrong with the network connections'
+        postWrapper.append(error)
+    })
 
-            postWrapper.append(error)
+    const form = document.querySelector('form')
+
+    form.addEventListener('submit', event => {
+        event.preventDefault()
+
+        const formData = new FormData(form)
+
+        const object = {}
+        formData.forEach((value, key) => {
+            object[key] = value
         })
 
+        const json = JSON.stringify(object)
+        console.log(json)
+
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: json,
+        }).then(res => res.json()).then(data => {
+            form.reset()
+            const postEl = document.createElement('div');
+            postEl.classList.add('post')
+            postEl.innerHTML += `
+                    <h4> <b>${data.id}</b> ${data.title}</h4>
+                    <p>${data.body}</p>
+                  `
+            postWrapper.append(postEl)
+        }).catch(err => {
+            console.log(err)
+        })
+    })
 })
 
 
